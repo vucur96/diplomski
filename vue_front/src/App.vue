cf0c1be0e5723be4663e7981213df76e29fdf65a
vucur96 <62611482+vucur96@users.noreply.github.com>
2024-10-25 19:36:02 +0200

    izmenjen stil tabela

--- vue_front/src/App.vue
+++ vue_front/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import MenuComponent from './views/menu.vue';
 </script>
 
 <template>
@@ -29,5 +28,62 @@ footer {
   text-align: center;
   margin-top: 2em;
 }
-</style>
 
+table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+th {
+  background-color: #1f83a1;
+  color: white;
+  padding: 10px;
+  text-align: left;
+  cursor: pointer;
+}
+
+th:hover {
+  background-color: #45a049;
+}
+
+td {
+  padding: 10px;
+  border: 1px solid #ddd;
+  text-align: left;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: #ddd;
+}
+
+input[type='text'] {
+  padding: 8px;
+  margin-bottom: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+}
+
+.btn-secondary {
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 10px;
+  cursor: pointer;
+  border-radius: 4px;
+}
+
+.btn-secondary:hover {
+  background-color: #45a049;
+}
+</style>
