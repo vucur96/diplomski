f053bc0faca88dd39712326607233b2990676769
vucur96 <62611482+vucur96@users.noreply.github.com>
2024-10-26 15:39:55 +0200

    Izmenjen meni

--- vue_front/src/views/menu.vue
+++ vue_front/src/views/menu.vue
@@ -4,16 +4,14 @@
     <RouterLink class="link"  to='/updateStudent' v-if="type=='student'"> Profil </RouterLink> 
     <RouterLink  class="link" to='/updateTeacher' v-if="type=='teacher'"> Profil</RouterLink> 
     <RouterLink class="link" v-if='type=="student"' to='/teachers'> Teachers </RouterLink> 
-    <RouterLink v-if='logedIn=="true"' class="link"  to='/archive'>History </RouterLink>
+    <RouterLink v-if="type=='student'|| type=='teacher'"  class="link"  to='/archive'>History </RouterLink>
     <RouterLink class="link" v-if='type=="teacher"' to='/lessonRequests'>Lesson requests </RouterLink>
     <RouterLink v-if='type=="teacher"' class='link' to='/teacher'> Home </RouterLink> 
     <RouterLink v-if='type=="student"' class='link' to='/student'> Home </RouterLink>
-    <div v-if="type=='admin'">
-    <RouterLink to='/teachers'>Teachers </RouterLink> 
-    <RouterLink to='/students'>Students </RouterLink> 
-    <RouterLink to='/addSubject'>New subjects </RouterLink> 
-    <RouterLink to='/adminHome'>Home </RouterLink>
-    </div>
+    <RouterLink v-if="type=='admin'" to='/teachers'>Teachers </RouterLink> 
+    <RouterLink v-if="type=='admin'" to='/students'>Students </RouterLink> 
+    <RouterLink v-if="type=='admin'" to='/addSubject'>New subjects </RouterLink> 
+    <RouterLink v-if="type=='admin'" to='/adminHome'>Home </RouterLink>
     <div >
         <div v-if='logedIn=="false"' style="align-items: right;"><button class="btn btn-primary" @click="login">Login</button></div>
         <div v-if='logedIn=="true"'><b-button class="btn btn-primary" @click="logout">Log out</b-button></div>
