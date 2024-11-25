f6320e9690a0219d90f4a2ee25ec9b19be272f2b
vucur96 <62611482+vucur96@users.noreply.github.com>
2024-11-25 14:44:00 +0100

    Novi pokusaj

--- vue_front/src/views/adminHome.vue
+++ vue_front/src/views/adminHome.vue
@@ -12,7 +12,12 @@
 
         <div>
             <h2>Broj nastavnika po predmetima:</h2>
-            <BarChart :chart-data="chartData" />
+            <div v-if="chartData.labels.length > 0">
+                <BarChart :chart-data="chartData" :options="chartOptions" />
+            </div>
+            <div v-else>
+                <p>Loading chart data...</p>
+            </div>
         </div>
 
         <table v-if='requests.length>0'>
@@ -96,6 +101,21 @@ export default {
     },
     components:{
         BarChart: Bar
-    }
+    },
+    mounted() {
+    SubjectService.GetTeachersPerSubject()
+        .then((response) => {
+            console.log('Response data:', response.data); // Verify the API response
+            const data = response.data;
+
+            this.chartData.labels = data.map((item) => item.subjectName);
+            this.chartData.datasets[0].data = data.map((item) => item.teacherCount);
+
+            console.log('Updated chartData:', this.chartData); // Check the transformed chartData
+        })
+        .catch((error) => {
+            console.error('Error fetching chart data:', error);
+        });
+},
 }
 </script>
